5393b5fa215b611378970f22193d7af935b166df
Manish Singh Rana <manish.manishr@gmail.com>
2024-04-29 14:04:06 -0700

    Script to upload Data completed

--- ImportFileFromFileShare.ps1
+++ ImportFileFromFileShare.ps1
@@ -9,6 +9,36 @@ $libraryPath = $migrationConfiguration.DestinationLibraryTitle
 $manifestFile = Import-Csv $migrationConfiguration.ManifestFile
 $fileCount = 1
 
+# Migration Log Output File
+$outputFile = ".\Logs\MigrationLog_$((Get-Date).ToString("yyyyMMddHHmmss")).csv"
+
+if(!(Test-Path -Path ".\Logs")){
+    New-Item -Path ".\Logs" -ItemType Directory | Out-Null
+}
+
+#CSV Header
+"Operation,Status,Source,Destination" | Out-File -FilePath $outputFile
+
+# Check for site Add and Customize Pages permission enabled for site. 
+function Check-PnPAdminSiteForScriptPermissions {
+    if([string]::IsNullOrEmpty($migrationConfiguration.DestinationCredentials.UserName)) {
+        Write-Host "Connecting to SPO Admin Center via interactive method" -ForegroundColor Green
+        Connect-PnPOnline $migrationConfiguration.TenantAdminUrl -Interactive
+    }else {
+        Write-Host "Connecting to SPO Admin Center via using config file credentials" -ForegroundColor Green
+        $securePasswordAdmin = ConvertTo-SecureString -AsPlainText $migrationConfiguration.DestinationCredentials.Password -Force
+        $credAdmin = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $migrationConfiguration.DestinationCredentials.UserName $securePassword
+        Connect-PnPOnline $migrationConfiguration.TenantAdminUrl -Credentials $cred
+    }
+
+    $site = Get-PnPTenantSite $migrationConfiguration.DestinationSite -Detailed
+    if($site.DenyAddAndCustomizePages -eq [Microsoft.Online.SharePoint.TenantAdministration.DenyAddAndCustomizePagesStatus]::Enabled){
+        # Scripting permissions need to be granted.
+        Write-Host "Disable site Deny Add & Customized Pages permissions at tenant level"
+        Set-PnPTenantSite $migrationConfiguration.DestinationSite -DenyAddAndCustomizePages:$false
+    }
+}
+
 # Connect to SharePoint Online using method defined in configuration file.
 if([string]::IsNullOrEmpty($migrationConfiguration.DestinationCredentials.UserName)) {
     Write-Host "Connecting to SPO via interactive method" -ForegroundColor Green
@@ -20,10 +50,14 @@ if([string]::IsNullOrEmpty($migrationConfiguration.DestinationCredentials.UserNa
     Connect-PnPOnline $migrationConfiguration.DestinationSite -Credentials $cred
 }
 
-
+# Uncomment following line if Add-PnPFile result in Access Denied error
+# Check-PnPAdminSiteForScriptPermissions
 
 foreach($csvRow in $manifestFile){
     $uploadPath = "$libraryPath"
+    $fileName = $csvRow."$($migrationConfiguration.FileNameMappingInCSV)"
+    $networkFileLocation = $csvRow."$($migrationConfiguration.NetworkFileLocationMappingInCSV)"
+    $metadata = $null
     
     # Set up folder migration details
     if($migrationConfiguration.UploadFolderHierarchy) {
@@ -31,22 +65,31 @@ foreach($csvRow in $manifestFile){
         $uploadPath = "$($libraryPath)/$($folderPath)"
     }
 
-    Write-Host "Uploading $fileCount of $($manifestFile.Count) to: $uploadPath"
+    Write-Host "Uploading $fileCount of $($manifestFile.Count) to: $uploadPath/$fileName"
 
     # Read file from network drive as a stream
+    $fileContent = Get-Content -Path $networkFileLocation -AsByteStream -Raw
+    $filestream = [System.IO.MemoryStream]::new($fileContent)
+    
+    # Setup metadata of uploaded file
+    if($migrationConfiguration.MetaDataMapping.Count -gt 0){
+        $metadata = @{}
 
-    $csvRow."$($migrationConfiguration.FileName)"
+        foreach($mapping in $migrationConfiguration.MetaDataMapping){
+            $metadata.Add($mapping.DestinationColumn, $csvRow."$($mapping.SourceColumn)")
+        }
+    }
 
-    #Add-PnPFile -Folder $libraryPath -FileName $fileName -Stream $stream -Values $metaData -Publish -PublishComment "Automated upload using PnP PowerShell migration script."
+    try {
+        $newFileInSPO = Add-PnPFile -Folder $uploadPath -FileName $fileName -Stream $filestream -Values $metadata -PublishComment "Automated upload using PnP PowerShell migration script."
+        "Upload,Success,$SourceFile,$($newFileInSPO.ServerRelativeUrl)" | Out-File -FilePath $outputFile -Append
+    }
+    catch {
+        <#Do this if a terminating exception happens#>
+        "Upload,Success,$SourceFile,#NA" | Out-File -FilePath $outputFile -Append
+    }
+    
     $fileCount++
 }
 
-try {
-    
-
-    
-}
-catch {
-    <#Do this if a terminating exception happens#>
-    #Error Log
-}
+Disconnect-PnPOnline
